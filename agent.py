b44cb82663da8435b56e2cdd97206e77a05e9324
Annis Souames <souames.annis@gmail.com>
2023-11-28 10:48:11 +0100

    Added support for swedish/svenska

--- agent.py
+++ agent.py
@@ -23,21 +23,28 @@ class Agent:
     def full_pipeline(self, audio_file: str) -> str:
         transc = self.whisper(audio_file)
         answers = self.ask_gpt(transc.text)
+
         print(answers)
         return answers.choices[0].message.content
 
     def ask_gpt(self, prompt):
+        prompt_sys = (
+            env["prompts"]["en"] if self.language == "en" else env["prompts"]["sv"]
+        )
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a helpful assistant in a scientific exposition/park for kids, keep your answers short.",
+                    "content": prompt_sys,
                 },
                 {"role": "user", "content": prompt},
             ],
         )
         return response
+
+    def ask_gpt_sv(self, prompt):
+        pass
 
     def whisper(self, filename):
         audio_file = open(filename, "rb")
